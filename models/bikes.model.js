2f9409fafdfe7c29748618b6531458a1df155e38
sachinjadhav <sachinjdv7@gmail.com>
2024-11-27 18:16:27 +0530

    added get api to frontend

--- models/bikes.model.js
+++ models/bikes.model.js
@@ -1,26 +1,29 @@
-import { Schema, model }  from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const ModelSchema = new Schema({
-    name: { type: String, required: true },
-    exShowroomPrice: { type: Number, required: true },
-    rto: { type: Number, required: true },
-    insurance: { type: Number, required: true },
-    hpa: { type: Number, required: false }, // Optional
-    others: { type: Number, required: false }, // Optional
-    accessories: [{ type: String }], // Array of accessories
-    pdi: { type: Number, required: false }, // Optional
-    total: { type: Number, required: true },
-    image: { type: String, required: false }, // URL or file path
-  }, { _id: false }); // _id: false to avoid generating sub-document IDs for models.
-  
+const ModelSchema = new Schema(
+  {
+    Name: { type: String, required: true },
+    'Ex-Showroom price': { type: Number, required: true },
+    RTO: { type: Number, required: true },
+    Insurance: { type: Number, required: true },
+    HPA: { type: Number, required: false }, // Optional
+    Others: { type: Number, required: false }, // Optional
+    Accessories: [{ type: String }], // Array of accessories
+    PDI: { type: Number, required: false }, // Optional
+    Total: { type: Number, required: true },
+    Image: { type: String, required: false }, // URL or file path
+  },
+  { _id: false }
+); // _id: false to avoid generating sub-document IDs for models.
 
-  const BikeSchema = new Schema({
+const BikeSchema = new Schema(
+  {
     name: { type: String, required: true },
     models: { type: [ModelSchema], required: true },
-  }, { timestamps: true });
-  
-  const Bike = model('Bike', BikeSchema);
-  
-export {
-    Bike
-}
+  },
+  { timestamps: true }
+);
+
+const Bike = model('Bike', BikeSchema);
+
+export { Bike };
